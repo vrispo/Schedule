3e55700103e933bb3716eb3183575b22cf630392
vrispo <veronica_rispo@hotmail.it>
2016-01-18 20:09:33 +0100

    aggiunta funzione che sottrae timespec e da risultato in ms

--- timeplus.c
+++ timeplus.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
+#include <math.h>
 
 #include "timeplus.h"
 
@@ -39,4 +40,15 @@ int time_cmp(struct timespec t1, struct timespec t2)
 	if(t1.tv_nsec < t2.tv_nsec)
 		return -1;
 	return 0;
+}
+
+void time_sub_ms(struct timespec t1, struct timespec t2, double* ms)
+{
+int	s,ns;
+
+	s=((t1.tv_sec)-(t2.tv_sec));
+	ns=((t1.tv_nsec)-(t2.tv_nsec));
+	ns+=(s* 1000000000);
+
+	*ms = fabs(ns * 0.000001);
 }
--- timeplus.h
+++ timeplus.h
@@ -12,5 +12,6 @@
 void time_copy(struct timespec *td, struct timespec ts);
 void time_add_ms(struct timespec *t, int ms);
 int time_cmp(struct timespec t1, struct timespec t2);
+void time_sub_ms(struct timespec t1, struct timespec t2, double* ms);
 
 #endif /* TIMEPLUS_H_ */
